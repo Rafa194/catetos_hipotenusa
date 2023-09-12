d99cd41c16ce5c73f698a107fe54c543612aa50d
Rafael Prado <rafaelnascimentoprado@hotmail.com>
2023-09-12 11:49:19 -0300

    first commit

---
+++ 017. Catetos e Hipotenusa.py
@@ -0,0 +1,11 @@
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hip = ((ca ** 2) + (co ** 2)) ** 0.5
+print('A hipotenusa vale {:.2f}'.format(hip))
+
+
+
+
+
+
+#h² = ca² + co²
